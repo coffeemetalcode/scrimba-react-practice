4eecdec70275a1dca2637e1512235b848a6252ae
dbstocker <dstocker@notesettersinc.com>
2018-12-26 19:07:59 -0500

    moving along

--- src/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Footer() {
-  return <footer>Copyright (c) 2018 coffee | metal | code</footer>;
-}
-
-export default Footer;
---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <span>
+        <input type="checkbox" />
+        Milk
+      </span><br />
+      <span>
+        <input type="checkbox" />
+        Eggs
+      </span><br />
+      <span>
+        <input type="checkbox" />
+        Bread
+      </span><br />
+    </div>
+  );
+}
+
+export default App;
