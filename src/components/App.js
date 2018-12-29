b13a9aba800ee0fbc72e9aabdc27cf4976f3108d
dbstocker <dstocker@notesettersinc.com>
2018-12-28 21:23:07 -0500

    16 segments left

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import TodoItem from "../components/TodoItem"
 import todosData from "../todosData"
 
 class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
 
   render() {
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>);
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>);
     return (
         <div className="todo-list">
             {todoItems}
